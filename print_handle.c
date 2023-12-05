39bc174948cb48a85963e5f61207a1cc6af06ee3
LFQIIIH <belaouadyounes13@gmail.com>
2023-12-05 21:16:43 +0100

    Print_handle : handle print

---
+++ print_handle.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buff: Buffer array to handle print.
+ * @flg: Calculates active flags
+ * @width: get width.
+ * @pre: Precision specification
+ * @size: Size specifier
+ *
+ * Return: 1 or 2;
+ */
+int handle_print(const char *fmt, int *ind, va_list list, char buff[],
+	int flg, int width, int pre, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buff, flg, width, pre, size));
+
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
