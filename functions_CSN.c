fcf7e588cf57fe0c532b397538a3860860c486b4
Elmahdi Ratil <elmahdiratildev@gmail.com>
2023-12-05 09:43:32 -0800

    add the function string

--- functions_CSN.c
+++ functions_CSN.c
@@ -18,3 +18,58 @@ int print_char(va_list types, char buff[],
 
 	return (handle_write_char(c, buff, flg, width, pre, size));
 }
+/*** PRINT A STRING ***/
+/**
+ * print_string - Prints a string
+ * @types: List a of arguments
+ * @buff: Buffer array to handle print
+ * @flg:  Calculates active flags
+ * @width: get width.
+ * @pre: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_string(va_list types, char buff[],
+	int flg, int width, int pre, int size)
+{
+	int length = 0, i;
+	char *str = va_arg(types, char *);
+
+	UNUSED(buff);
+	UNUSED(flg);
+	UNUSED(width);
+	UNUSED(pre);
+	UNUSED(size);
+	if (str == NULL)
+	{
+		str = "(null)";
+		if (pre >= 6)
+			str = "      ";
+	}
+
+	while (str[length] != '\0')
+		length++;
+
+	if (pre >= 0 && pre < length)
+		length = pre;
+
+	if (width > length)
+	{
+		if (flg & F_MINUS)
+		{
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
+	}
+
+	return (write(1, str, length));
+}
