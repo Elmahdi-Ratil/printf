0afe6ae8ffe6bf930a1729b63ba06107cad108e5
Elmahdi Ratil <elmahdiratildev@gmail.com>
2023-12-05 09:46:12 -0800

    add the function percent and int

--- functions_CSN.c
+++ functions_CSN.c
@@ -73,3 +73,70 @@ int print_string(va_list types, char buff[],
 
 	return (write(1, str, length));
 }
+/*** PRINT PERCENT SIGN ***/
+/**
+ * print_percent - Prints a percent sign
+ * @types: Lista of arguments
+ * @buff: Buffer array to handle print
+ * @flg:  Calculates active flags
+ * @width: get width.
+ * @pre: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_percent(va_list types, char buff[],
+	int flg, int width, int pre, int size)
+{
+	UNUSED(types);
+	UNUSED(buff);
+	UNUSED(flg);
+	UNUSED(width);
+	UNUSED(pre);
+	UNUSED(size);
+	return (write(1, "%%", 1));
+}
+
+/*** PRINT INT ***/
+/**
+ * print_int - Print int
+ * @types: Lista of arguments
+ * @buff: Buffer array to handle print
+ * @flg:  Calculates active flags
+ * @width: get width.
+ * @pre: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_int(va_list types, char buff[],
+	int flg, int width, int pre, int size)
+{
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
+
+	n = convert_size_number(n, size);
+
+	if (n == 0)
+		buff[i--] = '0';
+
+	buff[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
+
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
+
+	while (num > 0)
+	{
+		buff[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (write_number(is_negative, i, buff, flg, width, pre, size));
+}
+
