b3137b00f7af0d02965192f21e8bdb5b24e7d888
LFQIIIH <belaouadyounes13@gmail.com>
2023-12-05 21:15:09 +0100

    Get_FWPS : get precision

--- get_FWPS.c
+++ get_FWPS.c
@@ -69,4 +69,44 @@ int get_width(const char *format, int *i, va_list list)
 	return (width);
 }
 
+/**
+ * get_pre - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
+ */
+int get_pre(const char *format, int *i, va_list list)
+{
+	int curr_i = *i + 1;
+	int pre = -1;
+
+	if (format[curr_i] != '.')
+		return (pre);
+
+	pre = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			pre *= 10;
+			pre += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			pre = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (pre);
+}
+
 
