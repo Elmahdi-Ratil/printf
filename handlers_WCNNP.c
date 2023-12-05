ef3fc4548a0627d9a4fc126ee79b51345981adce
Elmahdi Ratil <elmahdiratildev@gmail.com>
2023-12-05 09:51:42 -0800

    add the handle write num

--- handlers_WCNNP.c
+++ handlers_WCNNP.c
@@ -76,3 +76,62 @@ int write_number(int is_negative, int ind, char buff[],
 	return (write_num(ind, buff, flg, width, pre,
 		length, padd, extra_ch));
 }
+/*** WRITE NUM ***/
+/**
+ * write_num - Write a number using a bufffer
+ * @ind: Index at which the number starts on the buffer
+ * @buff: Buffer
+ * @flg: Flags
+ * @width: width
+ * @pre: Precision specifier
+ * @length: Number length
+ * @padd: Pading char
+ * @extra_c: Extra char
+ *
+ * Return: Number of printed chars.
+ */
+int write_num(int ind, char buff[],
+	int flg, int width, int pre,
+	int length, char padd, char extra_c)
+{
+	int i, padd_start = 1;
+
+	if (pre == 0 && ind == BUFF_SIZE - 2 && buff[ind] == '0' && width == 0)
+		return (0); /* printf(".0d", 0)  no char is printed */
+	if (pre == 0 && ind == BUFF_SIZE - 2 && buff[ind] == '0')
+		buff[ind] = padd = ' '; /* width is displayed with padding ' ' */
+	if (pre > 0 && pre < length)
+		padd = ' ';
+	while (pre > length)
+		buff[--ind] = '0', length++;
+	if (extra_c != 0)
+		length++;
+	if (width > length)
+	{
+		for (i = 1; i < width - length + 1; i++)
+			buff[i] = padd;
+		buff[i] = '\0';
+		if (flg & F_MINUS && padd == ' ')/* Asign extra char to left of buffer */
+		{
+			if (extra_c)
+				buff[--ind] = extra_c;
+			return (write(1, &buff[ind], length) + write(1, &buff[1], i - 1));
+		}
+		else if (!(flg & F_MINUS) && padd == ' ')/* extra char to left of buff */
+		{
+			if (extra_c)
+				buff[--ind] = extra_c;
+			return (write(1, &buff[1], i - 1) + write(1, &buff[ind], length));
+		}
+		else if (!(flg & F_MINUS) && padd == '0')/* extra char to left of padd */
+		{
+			if (extra_c)
+				buff[--padd_start] = extra_c;
+			return (write(1, &buff[padd_start], i - padd_start) +
+				write(1, &buff[ind], length - (1 - padd_start)));
+		}
+	}
+	if (extra_c)
+		buff[--ind] = extra_c;
+	return (write(1, &buff[ind], length));
+}
