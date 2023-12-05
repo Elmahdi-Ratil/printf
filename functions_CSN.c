ee7484674a94b85ca97776f14d445e833d327325
Elmahdi Ratil <elmahdiratildev@gmail.com>
2023-12-05 09:47:26 -0800

    add the function binary

--- functions_CSN.c
+++ functions_CSN.c
@@ -139,4 +139,48 @@ int print_int(va_list types, char buff[],
 
 	return (write_number(is_negative, i, buff, flg, width, pre, size));
 }
+/***PRINT BINARY ***/
+/**
+ * print_binary - Prints an unsigned number
+ * @types: Lista of arguments
+ * @buff: Buffer array to handle print
+ * @flg:  Calculates active flags
+ * @width: get width.
+ * @pre: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of char printed.
+ */
+int print_binary(va_list types, char buff[],
+	int flg, int width, int pre, int size)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
 
+	UNUSED(buff);
+	UNUSED(flg);
+	UNUSED(width);
+	UNUSED(pre);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
+}
