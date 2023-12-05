3370640cca5aef6dfdd8db0da844a590b410e9bb
LFQIIIH <belaouadyounes13@gmail.com>
2023-12-05 21:22:24 +0100

    Utils_PAD_CSNU : check if it is digit

--- utils_PAD_CSNU.c
+++ utils_PAD_CSNU.c
@@ -38,4 +38,18 @@ int append_hexa_code(char ascii_code, char buff[], int i)
 	return (3);
 }
 
+/**
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
 
