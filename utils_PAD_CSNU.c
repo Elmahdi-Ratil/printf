e542c80c4e53ab930decab3f2a4f47d39db96947
LFQIIIH <belaouadyounes13@gmail.com>
2023-12-05 21:21:48 +0100

    Utils_PAD_CSNU : appending hexa ascii

--- utils_PAD_CSNU.c
+++ utils_PAD_CSNU.c
@@ -14,4 +14,28 @@ int is_printable(char c)
 	return (0);
 }
 
+/**
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buff: Array of chars.
+ * @i: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ *
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buff[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buff[i++] = '\\';
+	buff[i++] = 'x';
+
+	buff[i++] = map_to[ascii_code / 16];
+	buff[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
 
