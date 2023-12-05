72266837ad0ab8791d0ef2e0e0035ae5a6044f8c
Elmahdi Ratil <elmahdiratildev@gmail.com>
2023-12-05 09:50:54 -0800

    add the handle write number

--- handlers_WCNNP.c
+++ handlers_WCNNP.c
@@ -43,3 +43,36 @@ int handle_write_char(char c, char buff[],
 
 	return (write(1, &buff[0], 1));
 }
+/*** WRITE NUMBER ***/
+/**
+ * write_number - Prints a string
+ * @is_negative: Lista of arguments
+ * @ind: char types.
+ * @buff: Buffer array to handle print
+ * @flg:  Calculates active flags
+ * @width: get width.
+ * @pre: precision specifier
+ * @size: Size specifier
+ *
+ * Return: Number of chars printed.
+ */
+int write_number(int is_negative, int ind, char buff[],
+	int flg, int width, int pre, int size)
+{
+	int length = BUFF_SIZE - ind - 1;
+	char padd = ' ', extra_ch = 0;
+
+	UNUSED(size);
+
+	if ((flg & F_ZERO) && !(flg & F_MINUS))
+		padd = '0';
+	if (is_negative)
+		extra_ch = '-';
+	else if (flg & F_PLUS)
+		extra_ch = '+';
+	else if (flg & F_SPACE)
+		extra_ch = ' ';
+
+	return (write_num(ind, buff, flg, width, pre,
+		length, padd, extra_ch));
+}
